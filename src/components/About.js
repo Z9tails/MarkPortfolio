611911faec8bb27478aef256d7f487bdfdd8b508
Mark Taylor <100103488+Z9tails@users.noreply.github.com>
2022-10-14 17:11:14 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -18,8 +18,8 @@ export default function About (){
 
         <p class ="px-2">I am Mark! I'm a West Coast nomad currently based in Oregon. I'm a graduate from
              Oregon State University (2013) with a Bachelors of Science in Recreation Resource Management with a focus in 
-             Environmental Law (Water Rights and usage). My prior educational background includes Flight Technology,
-             Dive Technology, Electrical Engineering, and Pharmaceutical Technology.  I've spend the Covid-Times 
+             Environmental Law (Water Rights and Usage). My prior educational background includes Flight Technology,
+             Dive Technology, Electrical Engineering, and Pharmaceutical Technology.  I've spent the Covid-Times 
              substitute teaching, using mechanical and electrical engineering to repair and modify
              aircraft, culminating in my completion of UCB's Full Stack Programming bootcamp. I enjoy getting 
              to use my hands as much as my mind, especially in a symbiotic relationship where a cohort works towards
